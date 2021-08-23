542e4a47535207151379d22ff8acb6e3b56f2633
Aumlan Abrar <44971908+aumlan@users.noreply.github.com>
2021-08-23 23:28:32 +0600

    react component $ props core concept

--- src/index.js
+++ src/index.js
@@ -1,14 +1,40 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+// const index = 0;
+// const element = (
+//     <h1 className="heading" tabIndex={index}>
+//         <span className="text">Hello world</span>
+//         <img src="" alt="" />
+//     </h1>
+// );
+
+// function Text({ name }) {
+//     return (
+//         <h1 className="heading">
+//             <span className="text">Hello world {name}</span>
+//             <img src="" alt="" />
+//         </h1>
+//     );
+// }
+
+class Text extends React.Component {
+    render() {
+        return (
+            <h1 className="heading">
+                <span className="text">Hello world {this.props.name}</span>
+                <img src="" alt="" />
+            </h1>
+        );
+    }
+}
+
+// ReactDOM.render(<text name="aumlan" />, document.getElementById('root'));
+
+ReactDOM.render(<Text name="aumlan" />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
